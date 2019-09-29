c95540262fa0fdd8a2ac179e05a6bc1a1fda5c3c
sinhast <dylanv.dev@gmail.com>
2019-09-29 17:13:09 +0200

    Add the function to add score to player one in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const playerOneButton = document.querySelector(".playerOneButton");
 let scorePlayerOne = document.querySelector(".scorePlayerOne");
 let scoreOne = 0;
 
-const playerTwoButton =document.querySelector(".playerTwoButton");
+const playerTwoButton = document.querySelector(".playerTwoButton");
 let scorePlayerTwo = document.querySelector(".scorePlayerTwo");
 let scoreTwo = 0;
 
@@ -14,3 +14,9 @@ const winner = document.querySelector(".winnerName");
 
 const reset = document.querySelector(".resetScore");
 
+function addScorePlayerOne() {
+  if (scorePlayerOne.textContent != scoreLimit) {
+    scoreOne++;
+  }
+  scorePlayerOne.textContent = scoreOne;
+}
