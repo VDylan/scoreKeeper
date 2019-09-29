9e026777acc7a0d16dfe290b0d5017b5d5c52ef4
sinhast <dylanv.dev@gmail.com>
2019-09-29 18:52:00 +0200

    Creating the script for the dark mode in darkMode.js

---
+++ assets/js/darkMode.js
@@ -0,0 +1,16 @@
+const darkMode = document.querySelector(".darkMode");
+
+function changeTheme(ev) {
+  if (ev.target.tagName === "A") {
+    document.body.classList.toggle("darkMode");
+  }
+
+  if (document.querySelector("body").classList.contains("darkMode")) {
+    document.querySelector(".darkMode").classList.add("active");
+  } else {
+
+    document.querySelector(".darkMode").classList.remove("active");
+  }
+}
+
+darkMode.addEventListener("click", changeTheme);
