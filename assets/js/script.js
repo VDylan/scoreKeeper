48f36d2a8336d9cfb3fbdce6a91bd5a32e8bdc7e
sinhast <dylanv.dev@gmail.com>
2019-09-29 16:58:16 +0200

    Add all the required variable in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const playerOneButton = document.querySelector(".playerOneButton");
+let scorePlayerOne = document.querySelector(".scorePlayerOne");
+let scoreOne = 0;
+
+const playerTwoButton =document.querySelector(".playerTwoButton");
+let scorePlayerTwo = document.querySelector(".scorePlayerTwo");
+let scoreTwo = 0;
+
+let scoreToWin = document.querySelector(".scoreToWin");
+let scoreLimit = document.querySelector(".scoreLimit");
+const winner = document.querySelector(".winnerName");
+
+const reset = document.querySelector(".resetScore");
+
